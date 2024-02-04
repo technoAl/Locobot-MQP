812b3d26b1ec3254f5134207abd9aef9076f0164
technoAl <technoal9@gmail.com>
2024-02-04 14:04:56 -0600

    Added pour function

--- src/arm_tele.py
+++ src/arm_tele.py
@@ -13,11 +13,16 @@ class ArmController:
         rospy.Subscriber('arm/go_to', Pose, self.goTo)
         rospy.Subscriber('arm/grip', Bool, self.gripperUpdate)
         rospy.Subscriber('arm/pickplace', PoseArray, self.pickplace)
-	rospy.Subscriber('arm/pour', Bool, self.pour)
+	rospy.Subscriber('arm/follow', Pose, self.follow)
+	self.eePose = rospy.Publisher('arm/ee_pose', Pose);
         rospy.init_node('arm_controller', anonymous=True)
         self.robot = Robot('locobot')
         self.offset = [0.085, 0.0, 0.0]
         rospy.sleep(1)
+
+    def follow(self, msg):
+	pos = np.array([msg.position.x, msg.position.y, msg.position.z])
+	self.robot.arm.set_ee_pose(pos, self.iq, plan=False, numerical=False)	
 
     def goTo(self, msg):
         x = msg.position.x + self.offset[0]
@@ -53,7 +58,7 @@ class ArmController:
         self.robot.arm.go_home()
         rospy.sleep(0.1)
         self.robot.gripper.open()
-        self.goTo_method(object_pose.position.x-0.05, object_pose.position.y, 0.4)
+        self.goTo_method(object_pose.position.x, object_pose.position.y, 0.4)
         rospy.sleep(0.1)    
         self.goTo_method(object_pose.position.x, object_pose.position.y, object_pose.position.z)
         self.robot.gripper.close()
@@ -66,22 +71,30 @@ class ArmController:
         self.robot.gripper.open()
         rospy.sleep(0.1)
         self.goTo_method(target_pose.position.x, target_pose.position.y, 0.4)
-	
-    def pour(self, msg):
-	if msg.data:
-	    trans, rot, quat = self.robot.arm.pose_ee
-	    pose = {"position": np.array(trans), "pitch": 0, "roll": 1.57, "numerical":False}
-	    self.robot.arm.set_ee_pose_pitch_roll(**pose)
-	
 
     def run(self):
+	
         self.robot.gripper.open()
         self.robot.arm.go_home()
-        rospy.spin()
+        print(self.robot.arm.pose_ee)
+	self.iq = self.robot.arm.pose_ee[2]
+	r= rospy.Rate(30);
+	while not rospy.is_shutdown():
+		pose_msg = Pose()
+		ee = self.robot.arm.pose_ee[0]
+		q = self.robot.arm.pose_ee[2]
+		pose_msg.position.x = ee[0][0]
+		pose_msg.position.y = ee[1][0]
+		pose_msg.position.z = ee[2][0]
+		pose_msg.orientation.x = q[0]
+		pose_msg.orientation.y = q[1]
+		pose_msg.orientation.z = q[2]
+		pose_msg.orientation.w = q[3]
+		self.eePose.publish(pose_msg)
+		r.sleep()
 
 if __name__ == '__main__':
     try:
         ArmController().run()
-
     except rospy.ROSInterruptException:
         pass
--- src/arm_test.py
+++ src/arm_test.py
@@ -1,27 +1,28 @@
+#!/usr/bin/env python2.7
+
 import rospy
 from pyrobot import Robot
 import numpy as np
-from nav_msgs.msg import Path
-from tf.transformations import quaternion_from_euler
-from geometry_msgs.msg import Pose, PoseArray
-from std_msgs.msg import Bool
 
 class ArmTest:
+	
+	def __init__(self):
+		
+		self.robot = Robot('locobot')
+	
 
-    def __init__(self):
+	def rotate(self):
+		
+		pose = {"position": np.array([0.28, 0.17, 0.22]), "roll": 0.5, "numerical":False}
+		
+		self.robot.arm.set_ee_pose_pitch_roll(pose)
 
-        self.robot = Robot('locobot')
+	def run(self):
+		
+		self.rotate()
+		rospy.spin()
 
-    def rotate(self):
 
-        pose = {"position": np.array([0.28, 0.17, 0.22]), "roll": 0.5, "numerical": False}
-
-        self.robot.arm.set_ee_pose_pitch_roll(pose)
-
-    def run(self):
-        self.rotate()
-        rospy.spin()
-
-if __name__ == '__main__':
-    ArmTest.run()
-
+if __name__ == 'main':
+	ArmTest().run()
+	rospy.spin()
